58087bc18fc78e741af6c80c962995fe9d4f58ce
Bartosz <bartosz.zelek@protonmail.com>
2020-09-26 12:09:44 +0200

    Used API except web scrapping

--- ar.py
+++ ar.py
@@ -1,20 +1,32 @@
+# api https://rapidapi.com/Gramzivi/api/covid-19-data?endpoint=apiendpoint_90422c25-72f4-4e9a-a792-67e3dc7553a1
+
 import serial
 import time
-import pandas as pd
-arduino = serial.Serial('COM3', 9600)
-time.sleep(2)
+import requests
+import json
+arduino = serial.Serial('/dev/ttyUSB0', 9600) # set port
+time.sleep(10)
 
 while True:
     try:
-    	data = pd.read_html('https://www.worldometers.info/coronavirus/')[0]
-    	data = data.loc[data['Country,Other'] == 'Poland']
-    	sick = str(int(data['ActiveCases']))
-    	deaths = str(int(data['TotalDeaths']))
-    except:
+        url = "https://covid-19-data.p.rapidapi.com/country"
+        querystring = {
+            "format": "json",
+            "name": "poland" # set country
+        }
+        headers = {
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+            "x-rapidapi-key": "" # set api key
+        }
+        response = requests.get(url, headers=headers, params=querystring)
+        res = json.loads(response.text)[0]
+        sick = str(res["confirmed"] - res["recovered"])
+        deaths = str(res["deaths"])
+    except Exception as e:
+        # print(e)
         sick = "Error"
         deaths = "Error"
     dataToArduino = sick+','+deaths+"K"
-    print(dataToArduino)
+    # print(dataToArduino)
     arduino.write(bytes(dataToArduino, 'utf-8'))
-    time.sleep(600)
-    
+    time.sleep(900)
---
+++ corona_counter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/bartosz/dev/KoronaLicznik---Arduino-Python/ar.py
